207467c03e1b409457bc8a0101b7c359d7912932
Yaroslav <solomatin.yarik@mail.ru>
2025-04-14 22:53:45 +0300

    updated tests

--- src/half.js
+++ src/half.js
@@ -1,6 +1,5 @@
 const capitalize = (str) => {
     if (str === '') return '';
-    if (str === undefined) return undefined;
 
     const firstChar = str[0].toUpperCase();
     const restSubstring = str.slice(1);
--- tests/half.test.js
+++ tests/half.test.js
@@ -1,7 +1,13 @@
 import { capitalize } from '../src/half.js';
+import { strict as assert, deepEqual} from 'node:assert'
 
-if (capitalize('hello') !== 'Hello') {
-    throw new Error('Incorrect answer');
-}
+// if (capitalize('hello') !== 'Hello') {
+//     throw new Error('Incorrect answer');
+// }
+
+assert.equal(capitalize('hello'), 'Hello');
+assert.deepEqual(capitalize(''), '');
+// deepEqual нужен для глубокой проверки (не по ссылке), например для объектов
+// assert.deepEqual(capitalize({}), {}) / passed;
 
 console.log('All correct');
